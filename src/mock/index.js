c1a1cefdadcd969ec78b20a067236c2ac545fbfb
郭海生 <1358690951@qq.com>
2020-09-15 00:10:07 +0800

    首页布局

---
+++ src/mock/index.js
@@ -0,0 +1,11 @@
+import Mock from 'mockjs'
+import homeApi from './home'
+
+//设置延时200-2000毫秒
+Mock.setup({
+  timeout: '200-2000'
+})
+
+//首页相关
+//拦截/home/getData
+Mock.mock(/\/home\/getData/, 'get', homeApi.getHomeData)
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,8 +1,47 @@
 <template>
-  <div>home</div>
+  <el-row class="home" :gutter="20">
+    <el-col :span="8">
+      <el-card shadow="hover" style="height:290px">
+        鼠标悬浮时显示
+      </el-card>
+      <el-card shadow="hover" style="height:540px;margin-top:20px">
+        鼠标悬浮时显示
+      </el-card>
+    </el-col>
+    <el-col :span="16">
+      <div class="num">
+        <el-card shadow="hover" v-for="item in 6" :key="item">
+          <i class="icon"></i>
+          <div class="detail">
+            <p class="num">￥ 1234</p>
+            <p class="txt">今日支付订单</p>
+          </div>
+        </el-card>
+      </div>
+      <el-card shadow="hover">
+        <div style="height:280px"></div>
+      </el-card>
+      <div class="graph">
+        <el-card shadow="hover">
+          <div style="height:260px"></div>
+        </el-card>
+        <el-card shadow="hover">
+          <div style="height:260px"></div>
+        </el-card>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    this.$http('/home/getData').then(res => {
+      console.log(res.data)
+    })
+  }
+}
 </script>
-<style scoped></style>
+<style lang="scss" scoped>
+@import '~@/assets/scss/home.scss';
+</style>
