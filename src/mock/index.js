4a504ddefda05e502b8b398b53fecedd083ff6a1
郭海生 <1358690951@qq.com>
2020-09-17 23:47:51 +0800

    封装form和table

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,5 +1,6 @@
 import Mock from 'mockjs'
 import homeApi from './home'
+import userApi from './user'
 
 //设置延时200-2000毫秒
 Mock.setup({
@@ -8,4 +9,12 @@ Mock.setup({
 
 //首页相关
 //拦截/home/getData
+Mock.mock(/\/home\/getData/, 'get', homeApi.getStatisticalData)
+
+// 用户相关
+Mock.mock(/\/user\/getUser/, 'get', userApi.getUserList)
+Mock.mock(/\/user\/del/, 'get', userApi.deleteUser)
+Mock.mock(/\/user\/batchremove/, 'get', userApi.batchremove)
+Mock.mock(/\/user\/add/, 'post', userApi.createUser)
+Mock.mock(/\/user\/edit/, 'post', userApi.updateUser)
 Mock.mock(/\/home\/getData/, 'get', homeApi.getStatisticalData)
--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,8 +1,91 @@
 <template>
-  <div>user</div>
+  <div class="manage">
+    <div class="manage-header">
+      <el-button type="primary">+ 新增</el-button>
+      <common-form inline :formLabel="formLabel" :form="searchForm">
+        <el-button type="primary">搜索</el-button>
+      </common-form>
+    </div>
+    <div class="manage-content">
+      <common-table :tableData="tableData" :tableLabel="tableLabel" :config="config"></common-table>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import CommonForm from '@/components/CommonForm'
+import CommonTable from '@/components/CommonTable'
+export default {
+  components: {
+    CommonForm,
+    CommonTable
+  },
+  data() {
+    return {
+      tableData: [],
+      tableLabel: [
+        {
+          prop: 'name',
+          label: '姓名'
+        },
+        {
+          prop: 'age',
+          label: '年龄'
+        },
+        {
+          prop: 'sexLabel',
+          label: '性别'
+        },
+        {
+          prop: 'birth',
+          label: '出生日期'
+        },
+        {
+          prop: 'addr',
+          label: '地址'
+        }
+      ],
+      config: {
+        page: 1,
+        total: 30,
+        loading: false
+      },
+      searchForm: {
+        keyword: ''
+      },
+      formLabel: [
+        {
+          model: 'keyword',
+          label: '',
+          options: []
+        }
+      ]
+    }
+  },
+  methods: {
+    getList() {
+      this.config.loading = true
+      this.$http
+        .get('/api/user/getUser', {
+          params: {
+            page: this.config.page
+          }
+        })
+        .then(res => {
+          this.tableData = res.data.list.map(item => {
+            item.sexLabel = item === 0 ? '女' : '男'
+            return item
+          })
+          this.config.total = res.data.count
+          this.config.loading = false
+        })
+    }
+  },
+  created() {
+    this.getList()
+  }
+}
 </script>
-<style scoped></style>
+<style lang="scss" scoped>
+@import '@/assets/scss/common.scss';
+</style>
